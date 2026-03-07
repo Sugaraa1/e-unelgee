0e235805b602bcde09c68d64121ddba4c79946b7
sugar <iempty023@gmail.com>
2026-03-07 22:34:53 +0800

    4

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { RootNavigator } from '@navigation/RootNavigator';
+import { RootNavigator } from './src/navigation/RootNavigator';
+import { AuthProvider } from './src/store/authStore';
 
 export default function App() {
   return (
-    <>
+    <AuthProvider>
       <StatusBar style="dark" />
       <RootNavigator />
-    </>
+    </AuthProvider>
   );
 }
